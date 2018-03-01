1a7ed5cd62eb21d19e60e2cb0a2ca4a020bf0df6
ryandwalters <ryandwalters@gmail.com>
2018-03-01 16:05:00 -0600

    updated liri.js

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # liri-node-app
+
+Liri-node-app is an app that returns spotify, omdb, and twitter information based on command line entries in node.js.
+
+Twitter- displays your last 20 tweets
+
+node liri.js my-tweets
+ - 
+node liri.js movie-this '<movie name here>'
+Node liri.js spotify-this '<song name here>'
+node liri.js do-what-it-says
--- liri.js
+++ liri.js
@@ -14,7 +14,7 @@ var logText = function (text) {
 
   console.log(text)
   fs.appendFileSync("log.txt", text + "\n", "utf8");
- 
+
 }
 
 //user input
@@ -37,7 +37,6 @@ var readTwitter = function () {
     };
     twitterArray = tweets;
     for (var i = 0; i < twitterArray.length; i++) {
-
       logText("Time:" + twitterArray[i].created_at);
       logText("Text:" + twitterArray[i].text)
       logText('--------------------------------------');
@@ -46,10 +45,11 @@ var readTwitter = function () {
   });
 };
 
-
-
 //SPOTIFY
 var runSpotify = function (searchName) {
+  if (searchName === "") {
+    searchName = "The Sign Ace of Base";
+  };
 
   spotify.
     search({ type: "track", query: searchName }, function (err, data) {
@@ -59,28 +59,30 @@ var runSpotify = function (searchName) {
       }
 
       var song = data.tracks.items[0];
-      logText("-----Artists-----");
+      logText("--------------ARTIST------------------");
       logText(song.artists[0].name);
+      logText('--------------------------------------');
       //for (i=0; i <songs.artists.lenght; i++) {
 
       //}
-      logText("-----Song Name-------");
+      logText("-------------SONG NAME----------------");
       logText(song.name);
+      logText('--------------------------------------');
 
-      logText("----Preview Link-----");
+      logText("-------------PREVIEW LINK------------");
       logText(song.preview_url);
+      logText('--------------------------------------');
 
-      logText("-------Album----------")
+      logText("-------------ALBUM--------------------")
       logText(song.album.name);
-
+      logText('--------------------------------------');
 
     });
-
 };
 
 //OMDB
 
-var runOMDB = function () {
+var runOMDB = function (movieName) {
 
   // Include the request npm package (Don't forget to run "npm install request" in this folder first!)
   var request = require("request");
@@ -89,21 +91,9 @@ var runOMDB = function () {
   var nodeArgs = process.argv;
 
   // Create an empty variable for holding the movie name
-  var movieName = "";
+  //var movieName = "";
 
-  // Loop through all the words in the node argument
-  // And do a little for-loop magic to handle the inclusion of "+"s
-  for (var i = 3; i < nodeArgs.length; i++) {
-    if (movieName === "") {
 
-      movieName = nodeArgs[i];
-
-    } else if (movieName !== "") {
-
-      movieName = movieName + "+" + nodeArgs[i];
-    }
-
-  }
   if (movieName === "") {
     movieName = "mr+nobody"
 
@@ -146,184 +136,63 @@ var runOMDB = function () {
   });
 }
 
+//Random Do what it says
 
 var runRandom = function () {
-//RANDOM 
 
-// fs is a core Node package for reading and writing files
+  fs.readFile("random.txt", "utf8", function (error, data) {
 
+    // If the code experiences any errors it will log the error to the console.
+    if (error) {
+      return logText(error);
+    }
 
-// This block of code will read from the "movies.txt" file.
-// It's important to include the "utf8" parameter or the code will provide stream data (garbage)
-// The code will store the contents of the reading inside the variable "data"
-fs.readFile("random.txt", "utf8", function (error, data) {
+    // We will then print the contents of data
+    logText(data);
 
-  // If the code experiences any errors it will log the error to the console.
-  if (error) {
-    return logText(error);
-  }
+    // Then split it by commas (to make it more readable)
+    var dataArr = data.split(",");
+    switch (dataArr[0]) {
+      case "my-tweets":
+        readTwitter();
+        break;
 
-  // We will then print the contents of data
-  logText(data);
+      case "spotify-this-song":
+        runSpotify(dataArr[1]);
+        break;
 
-  // Then split it by commas (to make it more readable)
-  var dataArr = data.split(",");
-if (dataArr[0] === "spotify-this-song") {
-  runSpotify(dataArr[1]);
+      case "movie-this":
+        runOMDB(dataArr[1]);
+        break;
+
+    };
+    // if (dataArr[0] === "spotify-this-song") {
+    //   runSpotify(dataArr[1]);
+    // }
+    // We will then re-display the content as an array for later use.
+    logText(dataArr);
+
+  })
 }
-  // We will then re-display the content as an array for later use.
-  logText(dataArr);
-  
-
-})
-}
 
 
 
-
-  if (cmdIn === "tweet") {
+//SWITCH to determine which option to run
+switch (cmdIn) {
+  case "my-tweets":
     readTwitter();
-  } else if (cmdIn === "spotify") {
+    break;
+
+  case "spotify-this-song":
     runSpotify(searchStr);
-  } else if (cmdIn === "omdb") {
-    runOMDB();
-  } else if (cmdIn === "random") {
+    break;
+
+  case "movie-this":
+    runOMDB(searchStr);
+    break;
+
+  case "do-what-it-says":
     runRandom();
-  }
+    break;
 
-
-  
-//write to log.txt
-  
-
-
-
-
-
-// //------------------OMDB--------------------//
-// var request = require("request");
-
-// // Store all of the arguments in an array
-// var nodeArgs = process.argv;
-
-// // Create an empty variable for holding the movie name
-// var movieName = "";
-
-
-// // Loop through ll the words in the node argument
-// // And do a little for-loop magic to handle the inclusion of "+"s
-// for (var i = 2; i < nodeArgs.length; i++) {
-
-//   if (i > 2 && i < nodeArgs.length) {
-
-//     movieName = movieName + "+" + nodeArgs[i];
-//   }
-
-//   else {
-//     movieName += nodeArgs[i];
-
-//   }
-// }
-
-// // Then run a request to the OMDB API with the movie specified
-// var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-
-// // This line is just to help us debug against the actual URL.
-// logText(queryUrl);
-
-// request(queryUrl, function (error, response, body) {
-
-//   // If the request is successful
-//   if (!error && response.statusCode === 200) {
-
-//     // Parse the body of the site and recover just the imdbRating
-//     // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-//     //list of console logs from movie site
-//     logText("Release Year: " + JSON.parse(body).Year);
-//     logText()
-
-//     //   3. `node liri.js movie-this '<movie name here>'`
-
-//     //  * This will output the following information to your terminal/bash window:
-
-//     //    ```
-//     //      * Title of the movie.
-//     //      * Year the movie came out.
-//     //      * IMDB Rating of the movie.
-//     //      * Rotten Tomatoes Rating of the movie.
-//     //      * Country where the movie was produced.
-//     //      * Language of the movie.
-//     //      * Plot of the movie.
-//     //      * Actors in the movie.
-//   }
-// });
-
-
-
-
-
-// //-----------------Spotify------------------//
-
-// //. `node liri.js spotify-this-song '<song name here>'`
-// //   * This will show the following information about the song in your terminal/bash window
-
-// // * Artist(s)
-
-// // * The song's name
-
-// // * A preview link of the song from Spotify
-
-// // * The album that the song is from
-
-// // * If no song is provided then your program will default to "The Sign" by Ace of Base.
-
-
-//logText("#1 = " + searchStr);
-
-
-
-//code to allow multiple word queries for OMDB and SPOTIFY
-// var nodeArgs = process.argv;
-// var action = process.argv[2];
-// //Variable for movie or song requeset
-// var x = " ";
-// for (var i = 3; i < nodeArgs.length; i++) {
-
-//   if (i > 3 && i < nodeArgs.length) {
-//   x = x + "+" + nodeArgs[i];
-//   }
-//   else {
-//     x+= nodeArgs[i];
-//   }
-// }
-
-// //SWITCHES to specify which function to run
-// switch (action) {
-//   case "my-tweets":
-//     twitter();
-//     break;
-
-//   case "spotify-this-song":
-//   if (x) {
-//     spotify(x);
-//   } else {
-//     spotify ("The Sign")
-//   }
-//     break;
-
-//   case "movie-this":
-//   if (x) {
-//     omdb(x)
-//   } else {
-//     OMDB("Mr. Nobody");
-//     break;
-//   }
-
-//   case "do-what-it-says":
-//     random();
-//     break;
-
-//     default:
-//     logText("{Please enter a command: my-tweets, spotify-this-song, movie-this, do-what-it-says}");
-//   break;
-// 
+}
